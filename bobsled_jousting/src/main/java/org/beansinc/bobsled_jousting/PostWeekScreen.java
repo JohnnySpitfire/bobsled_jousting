747eac5d70e32df6e709f584baa6cfc04ab1c6d0
Rph1122 <130115005+Rph1122@users.noreply.github.com>
2023-05-22 18:15:59 +1200

    Items kinda work but its scuffed as shit

---
+++ bobsled_jousting/src/main/java/org/beansinc/bobsled_jousting/PostWeekScreen.java
@@ -0,0 +1,43 @@
+package org.beansinc.bobsled_jousting;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class PostWeekScreen {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PostWeekScreen window = new PostWeekScreen();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public PostWeekScreen() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
